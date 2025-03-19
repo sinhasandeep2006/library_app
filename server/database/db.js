82ca9d6bb8ee9ee3d8d7a66e003030635382b46a
sinha <1233sandeepsinha@gmail.com>
2025-03-19 22:57:59 +0530

    add the db connection to the env file

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,12 @@
 import mysql from "mysql2/promise";
-
+import dotenv from "dotenv";
+dotenv.config({ path: './config/config.env' });
 //  ===============creating the connections========================
 export const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Sinha123@gopal",
-  database: "library",
+  host: process.env.DB_HOST,
+       user: process.env.DB_USER,
+       password: process.env.DB_PASSWORD,
+       database: process.env.DB_NAME,
 });
 
 console.log("mysql connecte successfully");
